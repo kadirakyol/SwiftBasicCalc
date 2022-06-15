519bd2b546523dc147ed10de315d1cb48118a46f
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-15 17:52:02 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Kadir Akyol on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
