76f7b4f471e3ed58a12290c6519581c94d320cb1
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-16 15:43:42 +0300

    initial commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func toplama(_ sender: Any)
+    {
+        if let firstNumber = Int(firstText.text!)
+        {
+            if let secondNumber = Int(secondText.text!)
+            {
+                let result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func cikarma(_ sender: Any)
+    {
+        if let firstNumber = Int(firstText.text!)
+        {
+            if let secondNumber = Int(secondText.text!)
+            {
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func bölme(_ sender: Any)
+    {
+        if let firstNumber = Int(firstText.text!)
+        {
+            if let secondNumber = Int(secondText.text!)
+            {
+                let result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func carpma(_ sender: Any)
+    {
+        if let firstNumber = Int(firstText.text!)
+        {
+            if let secondNumber = Int(secondText.text!)
+            {
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    
 }
 
